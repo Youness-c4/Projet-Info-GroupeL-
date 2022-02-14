de8c7eb3ee90d8e01943dd7673f3cced6d03e029
sylvain-pierrot <47100292+sylvain-pierrot@users.noreply.github.com>
2022-02-14 14:31:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#Créer un tableau des personnages avec le JSON
+
+
+#FONCTIONS
+#fonction main déroulement de la partie
+
+#fonction selection du personnage (random)
+
+#fonction des questions généraliste avec critère en paramètre
+
+#fonction des questions "est-ce le personnage ... ?" avec personnage en paramètre
+
+#fonction éliminer un personnage
+
+
