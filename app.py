fd0518c33d2116bad57d32a0f0a22ba233e9bbc5
sylvain-pierrot <47100292+sylvain-pierrot@users.noreply.github.com>
2022-02-21 14:55:53 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter.ttk import Combobox
-from PIL import Image, ImageTk, ImageFilter
 
 import random
 import json
@@ -22,7 +21,7 @@ def defocus(event):
 # ---fenêtre de l'app----
 window = Tk()
 window.title("Qui est-tu ?")
-window.iconbitmap("logo2.ico")
+#window.iconbitmap("logo2.ico")
 window.config(background='#4a8ecc')
 
 # ----barre de menu de la fenêtre----
@@ -36,12 +35,16 @@ menu_bar.add_cascade(label="Fichier", menu=file_menu)
 window.config(menu=menu_bar)
 
 
-# ----palette du joueur----
+# ----palette de commandes du joueur----
 class Paddle:
     def __init__(self):
         # palette
-        self.paddle = Frame(window, bg='#599d8a')
+        self.paddle = Frame(window, bg='#4a8ecc')
         self.paddle.pack(expand=YES, padx=20, pady=20)
+
+        # ajout texte
+        l1 = Label(self.paddle, text="Le personnage possède-t-il", bg='#4a8ecc')
+        l1.grid(row=0, column=0, sticky=N + S + E + W)
 
         # ajouter liste déroulante des critères
         list_attr = []
@@ -51,14 +54,19 @@ class Paddle:
         list_combo = Combobox(self.paddle, values=list_attr, state="readonly")
         list_combo.current(0)
         list_combo.bind("<FocusIn>", defocus)
-        list_combo.pack()
+        list_combo.grid(row=0, column=1, sticky=N + S + E + W)
+
+        # bouton submit
+        button = Button(self.paddle, text="CONFIRMER", bg='#70d562', borderwidth=1, 
+            relief="raised", highlightthickness=0)
+        button.grid(row=0, column=2, padx=30)
 
 
 # ----plateau de jeu----
 class Board:
     def __init__(self, rows, columns):
         # plateau
-        self.board = Frame(window, bg='#003696')
+        self.board = Frame(window, bg='#4a8ecc')
         self.board.pack(expand=YES, padx=20, pady=20)
 
         # personnage qui-est-ce
@@ -75,10 +83,10 @@ class Board:
             for column in range(columns):
                 # boutons
                 image = PhotoImage(file=data['images'] + data["personnages"][i]["fichier"])
-                button = Button(self.board, image=image, bg='#003696',
-                                                   highlightthickness=10, relief=GROOVE, cursor="X_cursor")
+                button = Button(self.board, image=image, bg='#ffef00', border=10,
+                        highlightthickness=0, relief="raised", cursor="X_cursor", highlightbackground="#4a8ecc", highlightcolor="#4a8ecc")
                 button.configure(command=lambda b=button: self.eliminate(b))
-                button.grid(row=row, column=column, sticky=N + S + E + W)
+                button.grid(row=row, column=column, sticky=N + S + E + W, padx=10, pady=10)
                 # dictionnaire
                 self.personnages_board[button] = [False, data["personnages"][i], image]
                 # incr
@@ -88,7 +96,7 @@ class Board:
         if self.personnages_board[widget][0]:
             file = data['images'] + self.personnages_board[widget][1]["prenom"].lower()+".png"
             self.personnages_board[widget][0] = False
-            bg = '#003696'
+            bg = '#ffef00'
         else:
             file = data['images'] + self.personnages_board[widget][1]["prenom"].lower() + "_cross.png"
             self.personnages_board[widget][0] = True
