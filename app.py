fe495004f3b2bb0cd2ea5b4a263f0c1e57446577
sylvain-pierrot <47100292+sylvain-pierrot@users.noreply.github.com>
2022-03-25 17:12:53 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def restart():
 def defocus(event):
     event.widget.master.focus_set()
 
+
 # ---fenêtre de l'app----
 window = Tk()
 window.title("Qui est-ce ?")
@@ -35,20 +36,19 @@ window.config(menu=menu_bar)
 # ----variables globales----
 modefacile = IntVar()
 
-# ----palette de commandes du joueur----
-class Paddle:
-    def __init__(self, board):
 
+# ----tableau de bord du joueur----
+class Dashboard:
+    def __init__(self, gameb):
         # attributs déclarations
-        self.quiestce = board.quiestce
-        self.brd = board
+        self.gameboard = gameb
 
         # palette
-        self.paddle = Frame(window, bg='#4a8ecc')
-        self.paddle.pack(expand=YES, padx=20, pady=20)
+        self.interface = Frame(window, bg='#4a8ecc')
+        self.interface.pack(expand=YES, padx=20, pady=20)
 
         # ajout texte
-        l1 = Label(self.paddle, text="Le personnage possède-t-il", bg='#4a8ecc')
+        l1 = Label(self.interface, text="Le personnage possède-t-il", bg='#4a8ecc')
         l1.grid(row=0, column=0, sticky=N + S + E + W)
 
         # ajouter liste déroulante des critères
@@ -56,72 +56,57 @@ class Paddle:
         for key, value in data["personnages"][0].items():
             if key != "fichier" and key != "prenom":
                 list_attr.append(key)
-        list_combo = Combobox(self.paddle, values=list_attr, state="readonly")
+        list_combo = Combobox(self.interface, values=list_attr, state="readonly")
         list_combo.current(0)
         list_combo.bind("<FocusIn>", defocus)
         list_combo.grid(row=0, column=1, sticky=N + S + E + W)
 
         # bouton submit
-        button = Button(self.paddle, text="CONFIRMER", bg='#70d562', borderwidth=1,
+        button = Button(self.interface, text="CONFIRMER", bg='#70d562', borderwidth=1,
                         relief="raised", highlightthickness=0)
-        button.configure(command=lambda b=button: self.submitChoice(b, list_combo.get()))
+        #button.configure(command=lambda b=button: self.submitChoice(b, list_combo.get()))
+        button.configure(command=lambda: self.submitChoice(list_combo.get()))
         button.grid(row=0, column=2, padx=30)
 
         # label résultat
-        self.result = Label(self.paddle, text="", bg='#4a8ecc')
+        self.result = Label(self.interface, text="", bg='#4a8ecc')
         self.result.grid(row=0, column=3, padx=30)
 
         # bouton mode facile
-        buttonfacile = Checkbutton(self.paddle, text="MODE FACILE", variable=modefacile, onvalue=1,
+        checkbtn_facile = Checkbutton(self.interface, text="MODE FACILE", variable=modefacile, onvalue=1,
                                    offvalue=0, command=self.disabled_buttons)
-        buttonfacile.grid(row=0, column=4, padx=30)
-
+        checkbtn_facile.grid(row=0, column=4, padx=30)
 
     def disabled_buttons(self):
-        if modefacile.get()==1:
-            for x in self.brd.personnages_board:
-                self.brd.personnages_board[x][1].configure(state=DISABLED)
+        if modefacile.get() == 1:
+            for x in self.gameboard.personnages:
+                self.gameboard.personnages[x][1].configure(state=DISABLED)
         else:
-            for x in self.brd.personnages_board:
-                self.brd.personnages_board[x][1].configure(state=NORMAL)
+            for x in self.gameboard.personnages:
+                self.gameboard.personnages[x][1].configure(state=NORMAL)
         print("le mode facile est à", modefacile.get())
 
-
-    def submitChoice(self, widget, current):
-        print(self.quiestce[current])
-        self.result.configure(text=self.quiestce[current])
-        if modefacile.get()==1:
+    def submitChoice(self, current):
+        print(gameb.personnage_quiestce[current])
+        self.result.configure(text=gameb.personnage_quiestce[current])
+        if modefacile.get() == 1:
             for i in range(len(data['personnages'])):
                 answer = data['personnages'][i][current]
-                if answer != self.quiestce[current]:
-                    self.brd.eliminate(data['personnages'][i]["prenom"])
-
-
-
-# def cross(self, number):
-
-# file = data['images'] + perso["prenom"].lower()+"._cross.png"
-# print(file)
-# bg = '#D1007A'
-# img = PhotoImage(file=file)
-# widget.configure(bg=bg, image=img)
-# self.personnages_board[widget][2] = img
+                if answer != gameb.personnage_quiestce[current]:
+                    self.gameboard.eliminate(data['personnages'][i]["prenom"])
 
 
 # ----plateau de jeu----
-class Board:
+class Gameboard:
     def __init__(self, rows, columns):
         # plateau
-        self.board = Frame(window, bg='#4a8ecc')
-        self.board.pack(expand=YES, padx=20, pady=20)
+        self.interface = Frame(window, bg='#4a8ecc')
+        self.interface.pack(expand=YES, padx=20, pady=20)
 
         # attributs déclarations
-        self.personnages_board = {}
-        self.quiestce = ""
-
-        # personnage qui-est-ce
         nb_rand = random.randint(0, rows * columns - 1)
-        self.quiestce = data['personnages'][nb_rand]
+        self.personnage_quiestce = data['personnages'][nb_rand]
+        self.personnages = {}
 
         # initialisation plateau
         i = 0
@@ -130,46 +115,40 @@ class Board:
                 # boutons
                 prenom = data["personnages"][i]["prenom"]
                 image = PhotoImage(file=data['images'] + data["personnages"][i]["fichier"])
-                button = Button(self.board, image=image, bg='#ffef00', border=10,
+                button = Button(self.interface, image=image, bg='#ffef00', border=10,
                                 highlightthickness=0, relief="raised", cursor="X_cursor", highlightbackground="#4a8ecc",
                                 highlightcolor="#4a8ecc")
                 button.configure(command=lambda p=prenom: self.eliminate(p))
                 button.grid(row=row, column=column, sticky=N + S + E + W, padx=10, pady=10)
                 # dictionnaire
-                self.personnages_board[prenom] = [False, button, data["personnages"][i], image]
+                self.personnages[prenom] = [False, button, data["personnages"][i], image]
                 # incr
                 i += 1
 
     def eliminate(self, prenom):
-        # est éliminé
-        if self.personnages_board[prenom][0]:
-            if modefacile.get()==1:
-                pass
+        elimine = self.personnages[prenom][0]
+        file = data['images'] + self.personnages[prenom][2]["prenom"].lower()
+        if (elimine and modefacile.get() == 0) or (not elimine):
+            self.personnages[prenom][0] = not elimine
+            if not elimine:
+                file += "_cross.png"
+                bg = '#D1007A'
             else:
-                file = data['images'] + self.personnages_board[prenom][2]["prenom"].lower() + ".png"
-                self.personnages_board[prenom][0] = False
+                file += ".png"
                 bg = '#ffef00'
-                img = PhotoImage(file=file)
-                widget = self.personnages_board[prenom][1]
-                widget.configure(bg=bg, image=img)
-                self.personnages_board[prenom][3] = img
-        # pas éliminé
-        else:
-            file = data['images'] + self.personnages_board[prenom][2]["prenom"].lower() + "_cross.png"
-            self.personnages_board[prenom][0] = True
-            bg = '#D1007A'
             img = PhotoImage(file=file)
-            widget = self.personnages_board[prenom][1]
+            widget = self.personnages[prenom][1]
             widget.configure(bg=bg, image=img)
-            self.personnages_board[prenom][3] = img
+            self.personnages[prenom][3] = img
 
 
 # ----afficher la fenêtre---
 if __name__ == "__main__":
-    # création plateau + palette
-    bd = Board(int(data["ligne"]), int(data["colonne"]))
-    print("je suis le personnage à trouver : "+bd.quiestce["prenom"])
-    pd = Paddle(bd)
+    # création Gameboard + Dashboard
+    gameb = Gameboard(int(data["ligne"]), int(data["colonne"]))
+    dashb = Dashboard(gameb)
+
+    print("je suis le personnage à trouver : " + gameb.personnage_quiestce["prenom"])
 
     # MAJ taille fenêtre
     window.update()
