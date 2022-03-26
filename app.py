91003747a13957b59b9b5e7e5088fa8a131fc005
sylvain-pierrot <47100292+sylvain-pierrot@users.noreply.github.com>
2022-03-26 19:33:42 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def restart():
 def defocus(event):
     event.widget.master.focus_set()
 
+def gagne(dashb, gameb):
+    dashb.interface.pack_forget()
+    gameb.interface.pack_forget()
+    l = Label(window, text="YOU WIN", bg='#4a8ecc')
+    l.pack(expand=YES, padx=20, pady=20)
+
 
 # ---fenêtre de l'app----
 window = Tk()
@@ -30,28 +36,30 @@ file_menu = Menu(menu_bar, tearoff=0)
 file_menu.add_command(label="Nouvelle partie", command=restart)
 file_menu.add_command(label="Quitter", command=window.quit)
 menu_bar.add_cascade(label="Fichier", menu=file_menu)
+
 # configurer la fenêtre pour ajouter le menu
 window.config(menu=menu_bar)
 
 # ----variables globales----
-modefacile = IntVar()
+modefacile = BooleanVar()
+modefacile.set(False)
 
 
 # ----tableau de bord du joueur----
 class Dashboard:
     def __init__(self, gameb):
-        # attributs déclarations
+        # attributs
         self.gameboard = gameb
 
-        # palette
+        # Dashboard
         self.interface = Frame(window, bg='#4a8ecc')
         self.interface.pack(expand=YES, padx=20, pady=20)
 
-        # ajout texte
+        # label question
         l1 = Label(self.interface, text="Le personnage possède-t-il", bg='#4a8ecc')
         l1.grid(row=0, column=0, sticky=N + S + E + W)
 
-        # ajouter liste déroulante des critères
+        # liste déroulante question
         list_attr = []
         for key, value in data["personnages"][0].items():
             if key != "fichier" and key != "prenom":
@@ -61,38 +69,53 @@ class Dashboard:
         list_combo.bind("<FocusIn>", defocus)
         list_combo.grid(row=0, column=1, sticky=N + S + E + W)
 
-        # bouton submit
-        button = Button(self.interface, text="CONFIRMER", bg='#70d562', borderwidth=1,
+        # bouton submit question
+        button = Button(self.interface, text="CONFIRMER", bg='#4dbb69', borderwidth=1,
                         relief="raised", highlightthickness=0)
-        #button.configure(command=lambda b=button: self.submitChoice(b, list_combo.get()))
         button.configure(command=lambda: self.submitChoice(list_combo.get()))
         button.grid(row=0, column=2, padx=30)
 
-        # label résultat
-        self.result = Label(self.interface, text="", bg='#4a8ecc')
-        self.result.grid(row=0, column=3, padx=30)
+        # label résultat question
+        self.result_question = Label(self.interface, text="", bg='#4a8ecc')
+        self.result_question.grid(row=0, column=3, sticky=N + S + E + W)
 
-        # bouton mode facile
-        checkbtn_facile = Checkbutton(self.interface, text="MODE FACILE", variable=modefacile, onvalue=1,
-                                   offvalue=0, command=self.disabled_buttons)
-        checkbtn_facile.grid(row=0, column=4, padx=30)
+        # label selection personnage_quiestce
+        l2 = Label(self.interface, text="Je pense que le personnage est", bg='#4a8ecc')
+        l2.grid(row=1, column=0, sticky=N + S + E + W)
 
-    def disabled_buttons(self):
-        for x in self.gameboard.personnages:
-            if modefacile.get() == 1:
-                self.gameboard.personnages[x][1].configure(state=DISABLED)
-            else:
-                self.gameboard.personnages[x][1].configure(state=NORMAL)
-        print("le mode facile est à", modefacile.get())
+        # liste selection personnage_quiestce
+        list_pers = []
+        for elem in data["personnages"]:
+            list_pers.append(elem["prenom"])
+        list_combo2 = Combobox(self.interface, values=list_pers, state="readonly")
+        list_combo2.current(0)
+        list_combo2.bind("<FocusIn>", defocus)
+        list_combo2.grid(row=1, column=1, sticky=N + S + E + W)
+
+        # bouton submit selection perssonage_quiestce
+        button2 = Button(self.interface, text="CONFIRMER", bg='#4dbb69', borderwidth=1,
+                        relief="raised", highlightthickness=0)
+        button2.configure(command=lambda: self.quiestceTrouve(list_combo2.get()))
+        button2.grid(row=1, column=2, padx=30)
+
+        # label résultat selection personnage_quiestce
+        self.result_quiestce = Label(self.interface, text="", bg='#4a8ecc')
+        self.result_quiestce.grid(row=1, column=3, sticky=N + S + E + W)
 
     def submitChoice(self, attribut):
-        print(gameb.personnage_quiestce[attribut])
-        self.result.configure(text=gameb.personnage_quiestce[attribut])
+        self.result_question.configure(text=self.gameboard.personnage_quiestce[attribut])
         if modefacile.get() == 1:
             for i in range(len(data['personnages'])):
                 answer = data['personnages'][i][attribut]
                 if answer != gameb.personnage_quiestce[attribut]:
                     self.gameboard.eliminate(data['personnages'][i]["prenom"])
+
+    def quiestceTrouve(self, prenom):
+        if self.gameboard.personnage_quiestce["prenom"] == prenom:
+            gagne(self, self.gameboard)
+        else:
+            text = "NON"
+            self.result_quiestce.configure(text=text)
 
 
 # ----plateau de jeu----
@@ -140,12 +163,24 @@ class Gameboard:
             widget.configure(bg=bg, image=img)
             self.personnages[prenom][3] = img
 
+    def disabledGameb(self):
+        for x in self.personnages:
+            if modefacile.get():
+                self.personnages[x][1].configure(state=DISABLED)
+            else:
+                self.personnages[x][1].configure(state=NORMAL)
+
 
 # ----afficher la fenêtre---
 if __name__ == "__main__":
     # création Gameboard + Dashboard
     gameb = Gameboard(int(data["ligne"]), int(data["colonne"]))
     dashb = Dashboard(gameb)
+
+    # mode facile
+    modefacile_menu = Menu(menu_bar, tearoff=0)
+    modefacile_menu.add_checkbutton(label="activer", onvalue=1, offvalue=0, variable=modefacile, command=gameb.disabledGameb)
+    menu_bar.add_cascade(label="MODE FACILE", menu=modefacile_menu)
 
     print("je suis le personnage à trouver : " + gameb.personnage_quiestce["prenom"])
 
